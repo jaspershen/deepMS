ace85bd9c6129b5cd1b1ac95f0476f94fb922eef
David Fang <28679297+doawelul@users.noreply.github.com>
2019-06-18 11:40:10 -0700

    Clean up/Reformat code
    With respect to Google's R Style Guide:
    https://google.github.io/styleguide/Rguide.xml

--- R/trans.R
+++ R/trans.R
@@ -1,43 +1,45 @@
-trans <- function(data,#mzXML format data
-                  mz.range = c(50,1200),#mz range
-                  mz.pixel = 100,#mz partion
-                  rt.pixel = 100#rt partion
-                  ){
+trans <- function(data, mz.range, mz.pixel, rt.pixel) {
+  #
+  # Args:
+  #   data: mzXML format data
+  #   mz.range: mz range
+  #   mz.pixel: mz partion
+  #   rt.pixel: rt partion
+  #
+  # Returns:
+  #   New data matrix
   data1 <- mzR::openMSfile(data)
   data2 <- mzR::peaks(data1)
-  rm(list="data1")
-  ## build the rule of segment
+  rm(data1)
+  # Build the rule of segment
   mz1 <- mz.range[1]
   mz2 <- mz.range[2]
-  a <- seq(mz1, mz2, length.out = mz.pixel+1)
+  a <- seq(mz1, mz2, length.out = mz.pixel + 1)
   b <- a[1]
-  for (i in 2:(length(a)-1)) {
+  for (i in 2:(length(a) - 1)) {
     b = c(b, a[i], a[i])
   }
   b = c(b, a[length(a)])
-  ##c is the rule of mz pixel
+  # c is the rule of mz pixel
   c <- matrix(b, ncol = 2, byrow = TRUE)
-  remove(list=c("a","b"))
-  ###new data table for sample
+  rm(a, b)
+  # New data table for sample
   data.new <- matrix(0, ncol = length(data2), nrow = mz.pixel)
 
   #-----------------------------------------------------------------------------
   for (i in 1:length(data2)) {
-    if(i %% 100 == 0){
+    if (i %% 100 == 0){
       cat(paste(i, ""))
     }
+    
+    temp.data <- data2[[i]]
+    temp.idx <- whichgrid(num = temp.data[, 1], rule = c)
 
-  temp.data <- data2[[i]]
-  temp.idx <- whichgrid(num = temp.data[,1], rule = c)
-
-  for (j in 1:length(temp.idx)){
-    data.new[temp.idx[j],i] <- sum(temp.data[j,2], data.new[temp.idx[j],i])
-  }
-  rm(list = c("temp.data","temp.idx"))
+    for (j in 1:length(temp.idx)){
+      data.new[temp.idx[j], i] <- sum(temp.data[j, 2], data.new[temp.idx[j], i])
+    }
+    rm(temp.data, temp.idx)
   }
   #-----------------------------------------------------------------------------
   return(data.new)
 }
-
-
-

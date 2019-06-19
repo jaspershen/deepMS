5dffd68d24e3b51ea84bcd36b20b475778229566
David Fang <28679297+doawelul@users.noreply.github.com>
2019-06-19 16:47:04 -0700

    Clean up comments in imaging script

--- R/mzXML2PNG.R
+++ R/mzXML2PNG.R
@@ -9,12 +9,13 @@ map <- function(data) {
   data2 <- mzR::peaks(data1)
   rm(data1)
   numrows <- 0
+  # Find largest row number
   for(i in 1:length(data2)) {
     for(j in 1:dim(data2[[i]])[1]) {
       numrows = max(numrows, data2[[i]][j, 1])
     }
   }
-  # cat(paste("numrows is", numrows))
+  # Create the intensity plot
   plot <- matrix(0, ncol = length(data2)+1, nrow = numrows+1)
   for(i in 1:length(data2)) {
     mat <- data2[[i]]
@@ -28,6 +29,10 @@ map <- function(data) {
 }
 
 createPNG <- function(first, last) {
+  #
+  # Args:
+  #   first: number of the first file to image
+  #   last : number of the last file to image
   for(i in first:last) {
     input = paste(i, ".mzXML", sep = "")
     png(filename = paste(i, ".png", sep = ""), width = 256, height = 256)
@@ -36,9 +41,14 @@ createPNG <- function(first, last) {
   }
 }
 
-convert <- function(wd = "c:/New Folder",
+convert <- function(wd,
                     first = 1,
                     last = 10) {
+  #
+  # Args:
+  #   wd   : The working directory to read and output the files
+  #   first: The first file to image
+  #   last : The last file to image
   
   setwd(wd)
   createPNG(first, last)
